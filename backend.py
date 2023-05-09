8c5561fb4606a3b75aa6da99cb01d2429f68f742
TatUryeva <89226416+TatUryeva@users.noreply.github.com>
2023-05-08 17:47:23 -0700

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,28 +1,97 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
+
+import sqlite3
 
 app = Flask(__name__, template_folder='templates')
 
-events = []
+connect = sqlite3.connect('database.db')
+connect.execute('CREATE TABLE IF NOT EXISTS events (id INT, name VARCHAR, date DATE, time TIME)')
+cursor = connect.cursor()
+cursor.execute('SELECT id, name, date, time FROM events')
+events = cursor.fetchall()
+#events = []
 
 @app.route('/')
 def home():
-    return 'home'
+	
+	#cursor = connect.cursor()
+	cursor.execute('SELECT id, name, date, time FROM events')
+	events = cursor.fetchall()
+	
+	return render_template("frontend.html", events = events)
 	
 @app.route('/about')
 def about():
     return 'about'
 	
-@app.route('/submit_event', methods=['POST', 'GET'])
-def submit_event():
-    event_name = request.form.get('event name')
-    event_date = request.form.get('event date')
-    event_time = request.form.get('event time')
+@app.route('/add_event', methods=['post', 'get'])
+def add_event():
+    #event_name = request.form.get('event name')
+    #event_date = request.form.get('event date')
+    #event_time = request.form.get('event time')
+	event_name = request.form['event name']
+	event_date = request.form['event date']
+	event_time = request.form['event time']
 	
-    event_string = event_name+' on '+event_date+' at '+event_time
-    events.append(event_string)
-	#return event_name+' on '+event_date+' at '+event_time
-    #return render_template("frontend.html", event = event_string)	
-    return render_template("frontend.html", events = events)
+	#event_string = event_name+' on '+event_date+' at '+event_time
+	#events.append(event_string)
+	
+	db = sqlite3.connect("database.db")
+	cursor = db.cursor()
+	cursor.execute("select count(*) from events")
+	count = cursor.fetchone()[0] + 1
+	cursor.execute("INSERT INTO events (id,name,date,time) VALUES (?,?,?,?)",(count, event_name, event_date, event_time))
+	db.commit()
+	
+	cursor.execute('SELECT id, name, date, time FROM events')
+	events = cursor.fetchall()
+	
+	return render_template("frontend.html", events = events)
+
+@app.route('/edit_event/<int:value>')
+def change(value):
+    return render_template("edit.html", value = value)
+	
+@app.route('/change_event/<int:value>', methods=['post'])
+def change_event(value):
     
+    #new_name = request.form.get('change name')
+    #new_date = request.form.get('change date')
+    #new_time = request.form.get('change time')
+	new_name = request.form['change name']
+	new_date = request.form['change date']
+	new_time = request.form['change date']
+    
+    #new_string = new_name+' on '+new_date+' at '+new_time
+    #events.pop(value-1)
+    #events.insert(value-1, new_string)
+	
+	db = sqlite3.connect("database.db")
+	cursor = db.cursor()
+	cursor.execute("UPDATE EVENTS SET name=?, date=?, time=? WHERE id=?",(new_name, new_date, new_time, value))
+	db.commit()
+    
+	cursor.execute('SELECT id, name, date, time FROM events')
+	events = cursor.fetchall()
+	
+	return render_template("frontend.html", events = events)
+
+@app.route('/delete_event/<int:value>')
+def delete(value):
+	
+	#events.pop(value-1)
+	
+	db = sqlite3.connect("database.db")
+	cursor = db.cursor()
+	cursor.execute("DELETE FROM events WHERE id=?",[value])
+	db.commit()
+	
+	cursor.execute('SELECT id, name, date, time FROM events')
+	events = cursor.fetchall()
+	
+	return render_template("frontend.html", events = events)
+	
+	return redirect(url_for("frontend.html"))
+		
 if __name__ == "__main__":
     app.run(debug=True)
